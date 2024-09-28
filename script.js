69d64e5ce03bbd962501429a2abc5a61d65b39d0
Nevin <nevinramjitsingh0@gmail.com>
2024-09-27 21:54:37 -0400

    Major update
    Added new project, new resume, cleaned up the code, added transition animations, added button animations, added scroll for projects, updated hamburger menu

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+// Scroll Animations
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom >= 0
+    );
+  }
+  
+  function handleScrollAnimations() {
+    const sections = document.querySelectorAll('section');
+    
+    sections.forEach(section => {
+      if (isElementInViewport(section)) {
+        section.classList.add('appear');
+      } else {
+        section.classList.remove('appear');
+      }
+    });
+  }
+  
+  // Initial check on load
+  window.addEventListener('load', handleScrollAnimations);
+  
+  // Check on scroll
+  window.addEventListener('scroll', handleScrollAnimations);
